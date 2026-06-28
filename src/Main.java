8bd98af7ebeb63ce8d49ea3442ba5599bce7fcf3
abhishekgangrade <abhishekgangrade17@gmail.com>
2026-06-28 15:55:52 +0530

    Added few more practice questions.

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,6 @@
-import java.util.Comparator;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
+import java.security.Key;
+import java.util.*;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 import static java.util.Arrays.stream;
@@ -24,6 +23,16 @@ public class Main {
         m.numberOfEmployeesInEachDepartment(employees);
         m.findOldestEmployee(employees);
         m.youngestFemaleEmployee(employees);
+        m.ageGreaterAndLessThanThirty(employees);
+        m.departmentWithHighestEmployees(employees);
+        m.cityWithMaximumEmployees(employees);
+        m.anyEmployeeFromHRDept(employees);
+        m.distinctDepartmentNameThatEmployeesWorkFor(employees);
+        m.employeesLivesInBlore(employees);
+        m.numberOfEmployees(employees);
+        m.employeeCountInEveryDepartment(employees);
+        m.highestNumberOfEmployees1(employees);
+        m.sortByAgeAndName(employees);
 
 
 
@@ -104,6 +113,98 @@ public class Main {
                 .min(Comparator.comparingInt(Employee::getAge));
         System.out.println("Youngest female employee: " + youngestEmployee);
     }
+
+    public void ageGreaterAndLessThanThirty(List<Employee> li) {
+
+        Map<Boolean, List<Employee>> emp =  li.stream().collect(Collectors.partitioningBy(e -> e.getAge()>30));
+        System.out.println("Age greater than 30: " + emp);
+    }
+
+    public void departmentWithHighestEmployees(List<Employee> li){
+
+        Map<String, Long> dc = li.stream().collect(Collectors.groupingBy(Employee::getDeptName, Collectors.counting()));
+
+        Map.Entry<String, Long> result = dc.entrySet().stream().max(Map.Entry.comparingByValue())
+                .orElse(null);
+
+        System.out.println(result);
+
+
+
+    }
+
+    public void cityWithMaximumEmployees(List<Employee> li) {
+        Map<String, Long> cm = li.stream().collect(Collectors.groupingBy(Employee::getCity, Collectors.counting()));
+
+        Optional<Map.Entry<String, Long>> result  = cm.entrySet().stream().max(Map.Entry.comparingByValue());
+        System.out.println(result);
+
+    }
+
+    public void anyEmployeeFromHRDept(List<Employee> li) {
+
+        Map<String, List<Employee>> fromhrdept = li.stream().filter(n->n.getDeptName()=="HR").collect(Collectors.groupingBy(Employee::getDeptName));
+        System.out.println(fromhrdept);
+
+    }
+
+    public void distinctDepartmentNameThatEmployeesWorkFor(List<Employee> li){
+        Set<String> departments = li.stream()
+                .map(Employee::getDeptName)
+                .collect(Collectors.toSet());
+        System.out.println(departments);
+
+    }
+
+    //Find all employees who lives in ‘Blore’ city, sort them by their name and print the names of employees.
+    public void employeesLivesInBlore(List<Employee> li) {
+
+       List<Employee> emo = li.stream().filter(n -> n.getCity()=="Blore")
+                .sorted(Comparator.comparing(Employee::getName))
+                .collect(Collectors.toList());
+        System.out.println(emo);
+    }
+
+    //No of employees in the organisation.
+
+    public void numberOfEmployees(List<Employee> list){
+
+        List<Long> number = Collections.singletonList(list.stream().count());
+        System.out.println(number);
+    }
+
+    //Find employee count in every department
+    public void employeeCountInEveryDepartment(List<Employee> li) {
+
+        Map<String, Long> empCount = li.stream().collect(Collectors.groupingBy(Employee::getDeptName,Collectors.counting()));
+        System.out.println(empCount);
+    }
+
+    //Find the department which has the highest number of employees.
+    public void highestNumberOfEmployees1(List<Employee> li){
+
+        Map<String, Long> findCountFirst = li.stream().collect(Collectors.groupingBy(Employee::getDeptName, Collectors.counting()));
+        Optional<Map.Entry<String, Long>> fc = findCountFirst.entrySet().stream().max(Map.Entry.comparingByValue());
+        System.out.println(fc);
+
+
+
+    }
+
+    //Sorting a Stream by age and name fields.
+    public void sortByAgeAndName(List<Employee> li) {
+
+        List<Employee> employees = li.stream()
+                .sorted(Comparator.comparing(Employee::getAge)
+                        .thenComparing(Employee::getName))
+                .collect(Collectors.toList());
+
+        System.out.println(employees);
+    }
+
+
+
+
 
 
 
